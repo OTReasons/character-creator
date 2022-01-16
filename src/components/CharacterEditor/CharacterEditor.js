4eca71e900c65baa19d2f11792f36724ac06c029
Dmitriy Kuzmenkov <finalstaged@gmail.com>
2022-01-16 18:02:39 +0300

    Rendering Logic II Workshop solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -27,9 +27,10 @@ function App() {
   const [clothesColor, setClothesColor] = React.useState(
     defaultClothesColor
   );
-
+  
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveBar} />
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -77,7 +78,6 @@ function App() {
           />
         </div>
       </MaxWidthWrapper>
-
       <div className={styles.characterWrapper}>
         <Character
           body={body}
